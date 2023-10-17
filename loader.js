4cd0ae739da6848a31e3fd7ea48ab3fe356b236d
sanketvekariya95 <146871870+sanketvekariya95@users.noreply.github.com>
2023-10-17 17:15:09 +0530

    Upload Webiste

---
+++ loader.js
@@ -0,0 +1,9 @@
+const loader = document.getElementById("loader");
+let hed = document.getElementById("navbar")
+console.log(loader);
+const loadfunc = ()=>{
+    loader.style.display="none"
+    hed.style.display='block';
+}
+
+
